b3fcff83a5bb2c56b5de5ee81100305885c03ace
Michael <mickeymurage@gmail.com>
2022-09-13 04:32:52 +0300

    sizeof operator fixed letter n

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of a int: %d byte(s)\n", sizeof(3));
+	printf("Size of an int: %d byte(s)\n", sizeof(3));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
