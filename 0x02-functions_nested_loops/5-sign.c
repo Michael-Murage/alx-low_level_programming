bb46309181f5fa6dc1c15f711e06ad8831a06776
Michael <mickeymurage@gmail.com>
2022-09-15 02:06:02 +0300

    print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the character to check
+ * Return: 1 if positive, -1 if negative, 0 if zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -9,3 +9,5 @@
 3-islower.c checks for lowercase character.
 
 4-isalpha.c checks for alphabetic character.
+
+5-sign.c prints the sign of a number.
