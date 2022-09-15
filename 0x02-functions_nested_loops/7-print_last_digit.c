4d71b12862a23183c1a02be7ba6e93007c807b07
Michael <mickeymurage@gmail.com>
2022-09-15 05:33:31 +0300

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,11 @@ int print_last_digit(int n)
 	{
 		n = -n;
 	}
-	else
+	a = n % 10;
+	if (a < 0)
 	{
-		a = n % 10;
-		if (a < 0)
-		{
-			a = -a;
-			_putchar(a + '0');
-		}
+		a = -a;
 	}
+	_putchar(a + '0');
 	return (a);
 }
