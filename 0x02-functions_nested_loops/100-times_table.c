bdca49ca6257983f67a0fe3f13141f789be2c89f
Michael <mickeymurage@gmail.com>
2022-09-15 07:14:28 +0300

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,30 +15,41 @@ void print_times_table(int n)
 	{
 		return;
 	}
-	res = i * j;
-	if (j == 0)
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			res = i * j;
+			if (j == 0)
+			{
 				{
 					_putchar(res + '0');
-				} else if (res < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(res + '0');
-				} else if (res >= 10 && res < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				} else if (res >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((res / 100) + '0');
-					_putchar(((res / 10) % 10) + '0');
-					_putchar((res % 10) + '0');
 				}
+			}
+			else if (res < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(res + '0');
+			}
+			else if (res >= 10 && res < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res >= 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((res / 100) + '0');
+				_putchar(((res / 10) % 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+		}
+	}	
 }
