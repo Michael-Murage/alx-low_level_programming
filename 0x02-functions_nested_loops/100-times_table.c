e16cff0c40d5ff4cca65168c2850e0519115361b
Michael <mickeymurage@gmail.com>
2022-09-15 06:26:03 +0300

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,14 +7,14 @@
  */
 void print_times_table(int n)
 {
-	if (n > 15 || n < 0)
-	{
-		return;
-	}
 	int i;
 	int j;
 	int res;
 
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j < n; j++)
