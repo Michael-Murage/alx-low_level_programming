0299101a0c36fad2a33713c854881e210010a6a3
Michael <mickeymurage@gmail.com>
2022-09-15 20:42:54 +0300

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * Return: does not return anything
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,5 @@
 1-isdigit.c checks for a digit (0 through 9).
 
 2-mul.c multiplies two integers.
+
+3-print_numbers.c prints the numbers, from 0 to 9, followed by a new line.
