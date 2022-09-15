892d70cbd68a685e67fa302c4ae5d783d59749f1
Michael <mickeymurage@gmail.com>
2022-09-15 20:37:58 +0300

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a: first integer, multiplied
+ * @b: multiplier
+ * Return: returns the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 0-isupper.c checks for uppercase character.
 
 1-isdigit.c checks for a digit (0 through 9).
+
+2-mul.c multiplies two integers.
