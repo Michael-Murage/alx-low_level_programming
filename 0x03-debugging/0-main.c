1005a143f450fb8d0eee49f993a603315dfdbb68
Michael <mickeymurage@gmail.com>
2022-09-16 16:55:04 +0300

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests whether i is positive or negative
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	i = 70;
+	positive_or_negative(i);
+	return (0);
+}
