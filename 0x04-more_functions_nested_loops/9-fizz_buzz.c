6661b94494afd73cb207aad6391265f48ef29db9
Michael <mickeymurage@gmail.com>
2022-09-18 13:26:11 +0300

    started from 2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,9 +7,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	int i = 1;
+
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
