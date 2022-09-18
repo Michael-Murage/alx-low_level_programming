7f744437935d99a2b097e0233f81fe341e3f9639
Michael <mickeymurage@gmail.com>
2022-09-18 11:58:44 +0300

    testing checkers

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,38 @@
+
+
 #include <stdio.h>
 /**
- * main - prints 1-100 in fizz-buzz
- * Return: 0 success
- */
+* main - prints 1-100, fizz for 3, buzz for 5, fizzbuzz for multiples of 3, 5
+*
+* Return: Always 0
+*
+*/
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
+	printf("\n");
+	return (0);
 }
