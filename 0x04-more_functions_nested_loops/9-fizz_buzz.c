938f6c563c3f973a80388fc06bd0ee796d8ab5a3
Michael <mickeymurage@gmail.com>
2022-09-18 13:45:41 +0300

    fixed spaces

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,24 @@ int main(void)
 
 	i = 1;
 
-	printf("%d ", i);
+	printf("%d", i);
 	for (i = 2; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
