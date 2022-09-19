f45fd9b46c4094545a91083fff44c7e0e98a23eb
Michael <mickeymurage@gmail.com>
2022-09-19 21:25:27 +0300

    fixed possible error

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -11,7 +11,7 @@ int main(void)
     char *str;
     int len;
 
-    str = "My first strlen!";
+    str = "";
     len = _strlen(str);
     printf("%d\n", len);
     return (0);
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,7 +6,7 @@
  */
 int _strlen(char *s)
 {
-	int length;
+	int length = 0;
 
 	while (*s != '\0')
 	{
