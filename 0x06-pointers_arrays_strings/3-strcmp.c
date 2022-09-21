8fa220f321f1331bcf1c7d87fdcb367a58ff4c56
Michael <mickeymurage@gmail.com>
2022-09-21 22:14:16 +0300

    string comparisons fixed try with return 15

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -25,11 +25,11 @@ int _strcmp(char *s1, char *s2)
 			}
 			else if (s1[i] < s2[i])
 			{
-				return (-1);
+				return (-15);
 			}
 			else if (s1[i] > s2[i])
 			{
-				return (1);
+				return (15);
 			}
 		}
 	}
