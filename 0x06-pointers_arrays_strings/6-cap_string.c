79d00c8e57e0a29e9a0d87e666d2d606702f9298
Michael <mickeymurage@gmail.com>
2022-09-22 20:10:20 +0300

    capitalize string on conditions

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all characters of a string
+ * after ,;.!?"(){} space tab and new line
+ * @s: pointer to string being manipulated
+ * Return: resultant capitalized char*
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+		s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+		s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+		s[i] == '}' || s[i] == '\t' || s[i] == ' ' || s[i] == '\n')
+		&& (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+		{
+			s[i + 1] -= 32;
+		}
+		else
+		{
+			continue;
+		}
+
+	}
+	return (s);
+}
