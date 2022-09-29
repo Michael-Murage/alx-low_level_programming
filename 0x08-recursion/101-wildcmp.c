3dae673bcb74967f4d828556e9a022bf7577b9b3
Michael <mickeymurage@gmail.com>
2022-09-29 21:01:29 +0300

    two strings comparison

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * wildcmp - compares whether 2 strings can be considered identical
+ * @s1: address of first string
+ * @s2: address of second string
+ * Return: 1 if true, 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+
+}
