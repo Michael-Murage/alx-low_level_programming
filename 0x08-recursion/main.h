eabf205543e84343accc9ece844f19738ec3e165
Michael <mickeymurage@gmail.com>
2022-09-29 18:03:09 +0300

    print with recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-	_putchar(ch);
+	int _putchar(char ch);
 	void _puts_recursion(char *s);
-	_print_rev_recursion(char *s);
+	void _print_rev_recursion(char *s);
 #endif
